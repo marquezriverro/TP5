1b4bc66ad0ee607f4daf134f13eee08b06c65db0
Márquez Rivero <yaninamarquez090@gmail.com>
2024-08-30 15:24:17 -0300

    tp5

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const morgan = require("morgan");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+morgan(":method :url :status :res[content-length] - :response-time ms");
+
+const configuracion = require("./config.json");
+
+//cuando llega una peticion desde el cliente, debo redireccionar el pedido a su correspondiente controlador
+//en la URL tengo la informacion hacia donde enviar
+
+
+app.use("/api/medico", require("./controladores/medicoController.js"));
+
+
+//const pacienteController = require("./controladores/pacienteController.js");
+//app.use("/api/paciente", pacienteController);
+
+
+app.listen(configuracion.server.port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Sevidor encendido y escuchando en el puerto " + configuracion.server.port);
+  }
+});
