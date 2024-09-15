b101257e3e982502db12b38df5cb0cba435e8da2
Márquez Rivero <yaninamarquez090@gmail.com>
2024-09-15 17:50:19 -0300

    Update BD_conect.js

--- BD_conect.js/BD_conect.js
+++ BD_conect.js/BD_conect.js
@@ -1,1 +1,17 @@
+const mysql = require("mysql");
+const misconfiguraciones = require("config.json");
 
+const unanuevaconnection = mysql.createConnection(misconfiguraciones.database);
+
+
+
+unanuevaconnection.connect((err) => {
+    if (err) {
+        console.log(err.code);
+   const connection = mysql.createConnection(configuracion.database);
+ } else {
+        console.log("BD conectada");
+    }
+});
+
+module.exports = unanuevaconnection
